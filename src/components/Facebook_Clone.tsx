d8f4283b6b701ddb727911fe6b61875e5e52f233
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-10-18 10:18:45 +0530

    Update page title and improve layout in Facebook_Clone component

--- src/components/Facebook_Clone.tsx
+++ src/components/Facebook_Clone.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { ModeToggle } from "./theme/ModeToggle";
 
 const Facebook_Clone = () => {
   return (
-    <div className="bg-amber-50 dark:bg-amber-950">
-      Facebook_Clone <ModeToggle />
+    <div className="bg-amber-50 dark:bg-amber-950 min-h-screen flex items-center justify-center">
+      <ModeToggle />
     </div>
   );
 };
